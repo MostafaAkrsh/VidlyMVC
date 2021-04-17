26fc76137960e7e92e2fd399b1e6911eec0330b4
Mostafa Samir <mostafasamir355@gmail.com>
2021-04-17 17:14:50 +0200

    2

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -1,18 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Data.Entity;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
-using System.Data.Entity;
 using Vidly.ViewModels;
 
 namespace Vidly.Controllers
 {
     public class CustomersController : Controller
     {
-
         private ApplicationDbContext _context;
+
         public CustomersController()
         {
             _context = new ApplicationDbContext();
@@ -23,15 +20,15 @@ namespace Vidly.Controllers
             _context.Dispose();
         }
 
-
         public ActionResult New()
         {
-            var membershipTypes = _context.membershipTypes.ToList();
+            var membershipTypes = _context.MembershipTypes.ToList();
             var viewModel = new CustomerFormViewModel
             {
                 MembershipTypes = membershipTypes
-              };
-            return View("CustomerForm",viewModel);
+            };
+
+            return View("CustomerForm", viewModel);
         }
 
         [HttpPost]
@@ -45,7 +42,7 @@ namespace Vidly.Controllers
                 customerInDb.Name = customer.Name;
                 customerInDb.Birthdate = customer.Birthdate;
                 customerInDb.MembershipTypeId = customer.MembershipTypeId;
-                customerInDb.IsSubscribedToNewsLetter = customer.IsSubscribedToNewsLetter;
+                customerInDb.IsSubscribedToNewsletter = customer.IsSubscribedToNewsletter;
             }
 
             _context.SaveChanges();
@@ -55,19 +52,14 @@ namespace Vidly.Controllers
 
         public ViewResult Index()
         {
-            var customers = _context
-                            .Customers
-                            .Include(c => c.MembershipType)
-                            .ToList();
+            var customers = _context.Customers.Include(c => c.MembershipType).ToList();
 
             return View(customers);
         }
 
         public ActionResult Details(int id)
         {
-            var customer = _context.Customers
-                           .Include(c => c.MembershipType)
-                           .SingleOrDefault(c => c.Id == id);
+            var customer = _context.Customers.Include(c => c.MembershipType).SingleOrDefault(c => c.Id == id);
 
             if (customer == null)
                 return HttpNotFound();
@@ -78,17 +70,17 @@ namespace Vidly.Controllers
         public ActionResult Edit(int id)
         {
             var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
-                if (customer == null) return HttpNotFound();
 
-                var viewModel = new CustomerFormViewModel
-                {
-                    Customer = customer,
-                    MembershipTypes = _context.membershipTypes.ToList()
-                };
-                return View("CustomerForm",viewModel);
+            if (customer == null)
+                return HttpNotFound();
 
+            var viewModel = new CustomerFormViewModel
+            {
+                Customer = customer,
+                MembershipTypes = _context.MembershipTypes.ToList()
+            };
+
+            return View("CustomerForm", viewModel);
         }
-
-
     }
 }
--- Vidly/Models/Movie.cs
+++ Vidly/Models/Movie.cs
@@ -1,22 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
+﻿
+using System;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Web;
 
 namespace Vidly.Models
 {
     public class Movie
-    { 
+    {
         public int Id { get; set; }
+
         [Required]
         [StringLength(255)]
         public string Name { get; set; }
-        [Required]
+
         public Genre Genre { get; set; }
-        public int GenreId { get; set; }
-        public DateTime ReleaseDate { get; set; }
+
+        [Display(Name = "Genre")]
+        [Required]
+        public byte GenreId { get; set; }
+
         public DateTime DateAdded { get; set; }
-        public int NumberInStock { get; set; }
+
+        [Display(Name = "Release Date")]
+        public DateTime ReleaseDate { get; set; }
+
+        [Display(Name = "Number in Stock")]
+        public byte NumberInStock { get; set; }
     }
 }
